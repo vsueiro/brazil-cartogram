d98e31c8f6a4af980e5c0656cb8c4df20901b4f2
Vinicius Sueiro <hi@vsueiro.com>
2023-02-18 19:17:53 -0500

    Add empty Form class to handle user-controlled UI

---
+++ scripts/Form.js
@@ -0,0 +1,7 @@
+class Form {
+  constructor() {
+    console.log("created instace of Form");
+  }
+}
+
+export default Form;
--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+import Form from "./Form.js";
 import Data from "./Data.js";
 import Map from "./Map.js";
 import Color from "./Color.js";
 
-console.log(d3, new Data(), new Map(), new Color());
+console.log(d3, new Form(), new Data(), new Map(), new Color());
